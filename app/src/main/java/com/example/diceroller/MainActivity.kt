4843e9f63c319c66dbcca6b119a16b8a3946fd90
Lucidreline <casta.ma502@gmail.com>
2021-05-31 22:59:55 -0700

    A random number is now shown when the dice button is clicked.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
-            val toast = Toast.makeText(this, "The dice has been rolled BABY", Toast.LENGTH_SHORT) // message to show user
-            toast.show()
-        }
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        // val toast = Toast.makeText(this, "The dice has been rolled BABY", Toast.LENGTH_SHORT) // message to show user
+        // toast.show()
+
+        val myDice = Dice(6) // creates instance of a dice
+        val resultTextView: TextView = findViewById(R.id.textView) // finds the counter
+        resultTextView.text =
+            myDice.roll().toString() // changes the counter text to the random number
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
