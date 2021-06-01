7fdca2242f1731da7db5f4e6e5e9c09ad1bb4d9f
Lucidreline <casta.ma502@gmail.com>
2021-06-01 00:39:24 -0700

    Instead of displaying the random number, a dice image is shown to represent that random number.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice() // displays a random dice when the app first opens up
     }
 
     private fun rollDice() {
@@ -20,9 +23,20 @@ class MainActivity : AppCompatActivity() {
         // toast.show()
 
         val myDice = Dice(6) // creates instance of a dice
-        val resultTextView: TextView = findViewById(R.id.textView) // finds the counter
-        resultTextView.text =
-            myDice.roll().toString() // changes the counter text to the random number
+
+        val diceImage: ImageView = findViewById(R.id.imageView) // grabs the image view
+
+        val imageToUse = when(myDice.roll()){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(imageToUse) // sets the image to be the correct dice roll
+        diceImage.contentDescription = myDice.roll().toString() // lets people who use screenreaders to know the number
     }
 }
 
